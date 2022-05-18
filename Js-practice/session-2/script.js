e3aa308380ecc14575a83cda491f3cf51b4bbe7b
mukesh s <s.mukeshreddy2001@gmail.com>
2022-05-18 08:30:21 +0530

    update

--- Js-practice/session-2/script.js
+++ Js-practice/session-2/script.js
@@ -31,4 +31,19 @@ console.log(part)
 let text1="mukesh"
 let text2="Reddy"
 let text3="Sankalamaddi"
-console.log(text1.concat(text2,text3))
+console.log(text1.concat(" ",text2," ",text3))
+
+let nameStatement="hey my name is mukesh"
+let nameArray=nameStatement.split(" ")
+let charArray=nameStatement.split("")
+console.log(charArray)
+console.log(nameArray)
+
+const myFirstName = "mukesh"
+const mySecondName = "Reddy"
+console.log(`My name is ${myFirstName} ${mySecondName}`)
+
+let price = 10
+let vat =0.5123
+let total =`total: ${(price*(1+vat)).toFixed(1)}`
+console.log(total)
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Frontend
+### Frontend
 
-Pong-Game out-put-->[Pong-Game](https://mukeshsankalamaddi.github.io/Frontend/Pong-Game)
+## Pong-Game 
+    out-put-->[Pong-Game](https://mukeshsankalamaddi.github.io/Frontend/Pong-Game)
 
-Meditation-App out-put-->[Meditation-App](https://mukeshsankalamaddi.github.io/Frontend/meditation-app-master)
+## Meditation-App
+    out-put-->[Meditation-App](https://mukeshsankalamaddi.github.io/Frontend/meditation-app-master)
